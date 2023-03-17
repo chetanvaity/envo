e311b9e176281cca4cee51f4c0a56b0319451d52
Chetan Vaity <chetanv@gmail.com>
2023-03-17 10:27:15 -0700

    ssh config

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ENVO_DIR=${HOME}/sourc/chetanvaity/envo
+ENVO_DIR=${HOME}/source/chetanvaity/envo
 
 # Dirs
 mkdir -p ${HOME}/bin
@@ -7,6 +7,7 @@ mkdir -p ${HOME}/bin
 # Setup a new work Mac
 echo "Set up git..."
 # Install TBD
+rm -f ~/.gitconfig ~/.gitignore_global ~/bin/gcrb ~/bin/gp ~/bin/gpc ~/bin/gmu
 ln -s ${ENVO_DIR}/git/.gitconfig ${HOME}/.gitconfig
 ln -s ${ENVO_DIR}/git/.gitignore_global ${HOME}/.gitignore_global
 ln -s ${ENVO_DIR}/git/gcrb ${HOME}/bin/gcrb
@@ -15,11 +16,17 @@ ln -s ${ENVO_DIR}/git/gpc ${HOME}/bin/gpc
 ln -s ${ENVO_DIR}/git/gmu ${HOME}/bin/gmu
 
 echo "Setup bash..."
+rm -f ~/.bash_aliases ~/.bashrc ~/.bash_profile
 ln -s ${ENVO_DIR}/bash/.bash_aliases ${HOME}/.bash_aliases
 ln -s ${ENVO_DIR}/bash/.bash_myrc ${HOME}/.bashrc
 ln -s ${ENVO_DIR}/bash/.bash_profile ${HOME}/.bash_profile
 
+echo "Setup ssh..."
+rm -f ~/.ssh/config
+ln -s ${ENVO_DIR}/ssh/config ~/.ssh/config
+
 echo "Setup k8s..."
+rm -f ~/bin/kube-ps1.sh
 ln -s ${ENVO_DIR}/k8s/kube-ps1.sh ${HOME}/bin/kube-ps1.sh
 curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/amd64/kubectl" --output ${HOME}/bin/kubectl
 chmod 755 ${HOME}/bin/kubectl
